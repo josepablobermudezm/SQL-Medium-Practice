ef77aa98dcf6e9c9f7ea1005f56f5c63ba6e8569
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-10 02:19:55 -0600

    Time: 3638 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
 
+/*
+
+first long approach
+
 with a as (
 select 
     o.buyer_id, 
@@ -20,3 +24,16 @@ select
     0 as orders_in_2019
 from Users as u
 where u.user_id not in (select buyer_id from a)
+
+*/
+
+SELECT 
+    u.user_id AS buyer_id, 
+    join_date, 
+    COUNT(order_date) AS orders_in_2019 
+FROM Users as u
+LEFT JOIN
+Orders as o
+ON u.user_id = o.buyer_id
+AND YEAR(order_date) = '2019'
+GROUP BY u.user_id
